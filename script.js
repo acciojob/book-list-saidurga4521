4ab9cabe895b8e205167e40ed9aacbb749c651ef
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-01 12:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const titleE1=document.getElementById('title')
+const authorE1=document.getElementById('author')
+const ISBNE1=document.getElementById('isbn')
+const btnE1=document.getElementById('submit')
+let tbody=document.getElementById('book-list')
+btnE1.addEventListener('click',()=>{
+	var tr=document.createElement('tr')
+	var td1=document.createElement('td')
+	var td2=document.createElement('td')
+	var td3=document.createElement('td')
+	var btn=document.createElement('button')
+	td1.textContent = titleE1.value
+    td2.textContent = authorE1.value
+	td3.textContent = ISBNE1.value
+	btn.addEventListener('click', () => {
+        tbody.removeChild(tr)
+    })
+	tr.appendChild(td1)
+	tr.appendChild(td2)
+	tr.appendChild(td3)
+	tr.appendChild(btn)
+	tbody.appendChild(tr)
+	
+	
+})
